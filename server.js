755593952b1f498d48319f57a37c75560de088be
Luke Cartledge <luke@lukecartledge.com>
2016-08-15 16:56:53 +0100

    initiates node server with index.html

---
+++ server.js
@@ -0,0 +1,7 @@
+var httpServer = require("http-server");
+var path = require("path");
+
+var pathToHTMLAndJSFiles = path.join(__dirname, "./");
+var server = httpServer.createServer({ root: pathToHTMLAndJSFiles });
+server.listen(4567);
+console.log('Server is runnning locally on port 4567');
