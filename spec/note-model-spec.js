37e9f192b5e6b25384364081283d7c2827ce0b9c
Robert Summers <rsummers02@gmail.com>
2016-08-15 15:00:05 +0100

    passed first test

--- spec/note-model-spec.js
+++ spec/note-model-spec.js
@@ -1,4 +1,6 @@
 var note = new Note();
 var myNote = "My favorite language is Ruby";
 
+note.addNote(myNote);
+
 isTrue(note.showNote() === myNote );
--- src/note-model.js
+++ src/note-model.js
@@ -1,4 +1,18 @@
 (function(exports){
-  function Note(){}
+  function Note(){
+    _notes = []
+  };
+
+  Note.prototype.addNote = function(noteItem) {
+    _notes.push(noteItem);
+  };
+
+
+  Note.prototype.showNote = function() {
+    return _notes[0];
+  }
+
   exports.Note = Note;
+
+
 })(this);
