9228f4ba54c30f4df5a2515e496e444c03eedfda
Arunas Skirius <arukomp@gmail.com>
2016-08-17 11:29:45 +0100

    getting ready for challenge 6

--- spec/note-list-model-spec.js
+++ spec/note-list-model-spec.js
@@ -1,16 +1,15 @@
-function canCreateAlistOfNotes(){
-  var noteOne = "I love Ruby";
-  var noteTwo = "I don't love Javascript";
+describe("Note List Model", function() {
+  it("can create a list of notes", function() {
+    var noteOne = "I love Ruby";
+    var noteTwo = "I don't love Javascript";
 
-  var list = new List();
+    var list = new List();
 
-  list.addNote(noteOne);
-  list.addNote(noteTwo);
-  var testList = list.returnNotes();
-  isTrue(testList.length == 2);
-  isTrue(testList[0].showNote()=== noteOne);
-  isTrue(testList[1].showNote()=== noteTwo);
-
-}
-
-canCreateAlistOfNotes();
+    list.addNote(noteOne);
+    list.addNote(noteTwo);
+    var testList = list.returnNotes();
+    isTrue(testList.length == 2);
+    isTrue(testList[0].showNote()=== noteOne);
+    isTrue(testList[1].showNote()=== noteTwo);
+  });
+});
--- spec/note-list-view-spec.js
+++ spec/note-list-view-spec.js
@@ -1,16 +1,16 @@
-function returnsListView() {
-  var noteOne = "I love Ruby";
-  var noteTwo = "I don't love Javascript";
-  var list = new List();
-  list.addNote(noteOne);
-  list.addNote(noteTwo);
+describe("Note List View:", function() {
+  it("returns a list view", function() {
+    var noteOne = "I love Ruby";
+    var noteTwo = "I don't love Javascript";
+    var list = new List();
+    list.addNote(noteOne);
+    list.addNote(noteTwo);
 
-  var html = "<ul><li><div>I love Ruby</div></li><li><div>I don't love Javascript</div></li></ul>";
-  
-  var listView = new ListView(list);
+    var html = "<ul><li><div>I love Ruby</div></li><li><div>I don't love Javascript</div></li></ul>";
 
-  isTrue(listView.htmlList() === html);
+    var listView = new ListView(list);
 
-}
+    isTrue(listView.htmlList() === html);
 
-returnsListView();
+  });
+});
